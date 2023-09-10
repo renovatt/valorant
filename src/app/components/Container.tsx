bc5e0cc17942cdb30ab299053f8d0c511515f817
will <wlymes@gmail.com>
2023-09-10 14:40:37 -0300

    feat: container background

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,9 +1,21 @@
 import { ContainerTypeProps } from '@/@types'
 
 const Container = ({ children }: ContainerTypeProps) => {
+  const backgroundImageUrl = '/bg-wallpaper.jpg'
+
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundReteat: 'no-repeat',
+    backgroundPosition: 'center',
+  }
+
   return (
-    <main className="flex min-h-screen flex-1 flex-col items-center justify-center">
-      <section className="relative flex h-[90vh] w-[90%] items-center justify-center overflow-hidden rounded-lg bg-primary-900">
+    <main
+      style={sectionStyle}
+      className="flex min-h-screen flex-1 flex-col items-center justify-center bg-transparent"
+    >
+      <section className="bg-gradient-container relative flex h-[90vh] w-[90%] items-center justify-center overflow-hidden rounded-lg shadow-2xl">
         {children}
       </section>
     </main>
