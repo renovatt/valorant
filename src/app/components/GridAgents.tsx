5a423a4637dfc1737bf69c255179fa59c2d2a00b
will <wlymes@gmail.com>
2023-09-10 16:33:39 -0300

    feat: grid agents created

---
+++ src/app/components/GridAgents.tsx
@@ -0,0 +1,25 @@
+import AgentsCard from './AgentsCard'
+
+const GridAgents = () => {
+  return (
+    <article className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-8">
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+      <AgentsCard />
+    </article>
+  )
+}
+export default GridAgents
