204fb5e0367b283c8bebf552843f7c945d4e8cfb
will <wlymes@gmail.com>
2023-09-10 21:53:28 -0300

    feat: types added

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -21,3 +21,59 @@ export type ViewContainerProps = {
 export type ViewContentProps = {
   children: ReactNode
 }
+
+export type ReactQueryProviderProps = {
+  children: ReactNode
+}
+
+export interface Role {
+  uuid: string
+  displayName: string
+  description: string
+  displayIcon: string
+  assetPath: string
+}
+
+export interface Ability {
+  slot: string
+  displayName: string
+  description: string
+  displayIcon: string
+}
+
+export interface MediaList {
+  id: number
+  wwise: string
+  wave: string
+}
+
+export interface VoiceLine {
+  minDuration: number
+  maxDuration: number
+  mediaList: MediaList[]
+}
+
+export interface Agent {
+  uuid: string
+  displayName: string
+  description: string
+  developerName: string
+  displayIcon: string
+  displayIconSmall: string
+  bustPortrait: string
+  fullPortrait: string
+  fullPortraitV2: string
+  killfeedPortrait: string
+  background: string
+  backgroundGradientColors: string[]
+  assetPath: string
+  isFullPortraitRightFacing: boolean
+  isPlayableCharacter: boolean
+  isAvailableForTest: boolean
+  isBaseContent: boolean
+  role: Role
+  abilities: Ability[]
+  voiceLine: VoiceLine
+}
+
+export type AgentsCardProps = Partial<Agent>
