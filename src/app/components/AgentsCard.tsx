70ef879349f691fe9a04b1c42998cb48d6c260c7
will <wlymes@gmail.com>
2023-09-10 21:59:29 -0300

    refactor: agents card refactored

--- src/app/components/AgentsCard.tsx
+++ src/app/components/AgentsCard.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import Image from 'next/image'
+import { AgentsCardProps } from '@/@types'
 
-const AgentsCard = () => {
+const AgentsCard = ({
+  displayName,
+  abilities,
+  fullPortrait,
+}: AgentsCardProps) => {
+  const backgroundImageUrl = '/bg-card.png'
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundReteat: 'no-repeat',
+    backgroundPosition: 'center',
+  }
   return (
-    <section className="m-1 mx-2 flex h-48 w-32 items-center justify-center rounded-md border border-black bg-white p-1 md:h-52 md:w-36">
-      <h2>Card</h2>
+    <section className="bg-gradient-card relative mx-2 my-8 flex h-48 w-32 items-center justify-end border-2 border-primary-700 bg-opacity-50  p-[2px] hover:cursor-pointer md:my-6 md:h-52 md:w-36">
+      <div
+        style={sectionStyle}
+        className="absolute bottom-0 h-full w-full opacity-20"
+      ></div>
+      <section className="flex h-full w-full items-center justify-center border-2 border-primary-900">
+        <figure className="absolute bottom-0 z-10 flex h-64 w-full items-center justify-center transition-all ease-in-out hover:scale-110">
+          <Image
+            src={fullPortrait ?? ''}
+            alt={displayName ?? ''}
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
+            priority
+          />
+        </figure>
+
+        <article
+          className="pointer-events-none absolute z-50 flex h-full
+       w-full flex-col items-center justify-end pb-3"
+        >
+          <h1 className="my-1 text-2xl font-semibold uppercase text-white shadow-black drop-shadow-lg">
+            {displayName}
+          </h1>
+          <section className="flex items-center justify-normal gap-3">
+            {abilities?.map(
+              (abilities, index) =>
+                abilities.displayIcon && (
+                  <figure key={index} className="flex">
+                    <Image
+                      src={abilities.displayIcon}
+                      alt={displayName ?? ''}
+                      width={100}
+                      height={100}
+                      className="h-4 w-4"
+                    />
+                  </figure>
+                ),
+            )}
+          </section>
+        </article>
+      </section>
     </section>
   )
 }
--- src/app/components/GridAgents.tsx
+++ src/app/components/GridAgents.tsx
@@ -1,24 +1,14 @@
+'use client'
+import { useAgents } from '@/hooks/useAgents'
 import AgentsCard from './AgentsCard'
 
 const GridAgents = () => {
+  const { data, isLoading, isError } = useAgents()
   return (
-    <article className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-8">
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
-      <AgentsCard />
+    <article className="mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-8">
+      {isLoading && <p className="text-white">Carregando..</p>}
+      {isError && <p className="text-white">Aconteceu algum erro!</p>}
+      {data && data?.map((agent) => <AgentsCard key={agent.uuid} {...agent} />)}
     </article>
   )
 }
