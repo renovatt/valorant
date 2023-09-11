7bf5e9b24c660f624ea7d6a165d6da541d3e62b7
will <wlymes@gmail.com>
2023-09-11 12:55:50 -0300

    feat: modal container

---
+++ src/app/components/ModalContainer.tsx
@@ -0,0 +1,22 @@
+import { ModalContainerProps } from '@/@types'
+
+const ModalContainer = ({ children }: ModalContainerProps) => {
+  const backgroundImageUrl = '/bg-card.png'
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundReteat: 'no-repeat',
+    backgroundPosition: 'center',
+  }
+  return (
+    <section className="bg-gradient-modal-container relative m-auto flex h-[80%] w-[80%] flex-col items-center justify-center rounded-lg shadow-xl shadow-zinc-950 md:h-[80%]">
+      <div
+        style={sectionStyle}
+        className="absolute bottom-0 h-full w-full opacity-10"
+      ></div>
+      {children}
+    </section>
+  )
+}
+
+export default ModalContainer
