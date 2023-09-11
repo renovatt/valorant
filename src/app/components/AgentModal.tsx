0027191908959365199df38fb4f4935698434c23
will <wlymes@gmail.com>
2023-09-11 12:56:55 -0300

    feat: agent modal

--- src/app/components/AgentModal.tsx
+++ src/app/components/AgentModal.tsx
@@ -1,18 +1,51 @@
 'use client'
-import { AgentModal } from '@/@types'
+import DesktopModal from './DesktopModal'
 import { useRouter } from 'next/navigation'
+import { AgentModalProps } from '@/@types'
+import MobileModal from './MobileModal'
+import ModalContainer from './ModalContainer'
+import { TiArrowBackOutline } from 'react-icons/ti'
 
-const AgentModal = ({ displayName }: AgentModal) => {
+const AgentModal = ({
+  displayName,
+  background,
+  role,
+  description,
+  fullPortrait,
+  abilities,
+  voiceLine,
+}: AgentModalProps) => {
   const router = useRouter()
   const backToAgents = () => {
     router.push('/views/agents')
   }
 
   return (
-    <div className="h-40 w-40">
-      <h1 className="text-white">{displayName}</h1>
-      <button onClick={backToAgents}>Voltar</button>
-    </div>
+    <ModalContainer>
+      <TiArrowBackOutline
+        onClick={backToAgents}
+        className="absolute right-5 top-4 h-7 w-7 text-white transition-all ease-in hover:cursor-pointer hover:text-primary-700"
+      />
+      <DesktopModal
+        abilities={abilities}
+        description={description}
+        fullPortrait={fullPortrait}
+        voiceLine={voiceLine}
+        displayName={displayName}
+        role={role}
+        background={background}
+      />
+
+      <MobileModal
+        abilities={abilities}
+        description={description}
+        fullPortrait={fullPortrait}
+        voiceLine={voiceLine}
+        displayName={displayName}
+        role={role}
+        background={background}
+      />
+    </ModalContainer>
   )
 }
 
