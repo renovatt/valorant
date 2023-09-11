d0f24d428a3a267b068c65accbf7e297259ca302
will <wlymes@gmail.com>
2023-09-10 21:56:36 -0300

    build: remote patterns config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'media.valorant-api.com',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
