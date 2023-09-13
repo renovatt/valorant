741c8bb004f11c77c9b44d48f1a53ec8c85f0020
will <wlymes@gmail.com>
2023-09-13 13:12:07 -0300

    feat: thumbnail page created

---
+++ src/app/components/Thumbnail.tsx
@@ -0,0 +1,41 @@
+'use client'
+import Image from 'next/image'
+import thumb from '../../../public/thumb.png'
+import logo from '../../../public/valorant-logo.svg'
+import Link from 'next/link'
+
+const Thumbnail = () => {
+  const handleLoadingImage = (
+    event: React.SyntheticEvent<HTMLImageElement>,
+  ): void => {
+    event.currentTarget.style.opacity = '1'
+  }
+  return (
+    <section className="bg-gradient-weapon-card animate__fadeIn animate__animated relative flex h-full w-[80%] items-center justify-center rounded-lg md:h-full md:w-[90%]">
+      <Image
+        src={logo}
+        alt="valorant-logo"
+        className="reflection mt-24 opacity-30 md:mt-16"
+        priority
+      />
+      <figure className="z-20" data-aos="zoom-in">
+        <Image
+          src={thumb}
+          alt="valorant-logo"
+          className="object-cover opacity-0 md:h-96 md:w-96"
+          priority
+          onLoad={handleLoadingImage}
+        />
+      </figure>
+
+      <Link
+        href={'/views/agents'}
+        className="center-absolute z-50 w-[90%] max-w-xs border-2 border-primary-700 py-2 text-center text-2xl uppercase text-primary-700 transition-all
+      ease-in-out hover:border-white hover:text-white"
+      >
+        Eliminar
+      </Link>
+    </section>
+  )
+}
+export default Thumbnail
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import Title from './components/Tiltle'
+import Thumbnail from './components/Thumbnail'
 import ViewContainer from './components/Containers/ViewContainer'
 import ViewContent from './components/Containers/ViewContent'
-import Title from './components/Tiltle'
 
 export default function Home() {
   return (
     <ViewContainer>
       <Title title="Inicio" />
       <ViewContent>
-        <h1>Dashboard</h1>
+        <Thumbnail />
       </ViewContent>
     </ViewContainer>
   )
